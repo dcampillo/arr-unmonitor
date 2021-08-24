d7fc54503db721492f3a5170ce11c855fe647770
david <david@debnzb.zorglub.info>
2021-08-24 16:02:46 +0200

    - Update radarr-unmonitor.py

--- radarr-unmonitor.py
+++ radarr-unmonitor.py
@@ -9,7 +9,7 @@
 
 import os, time, shutil, sys, logging
 import requests, json
-import json
+import syslog
 
 from os import environ, path
 
@@ -23,44 +23,43 @@ def getMovie(movieID):
 
 def setMonitoring(movieID, qualityId, moviePath, MonitorStatus):
     apireq = "{0}/api/v3/movie/{1}?moveFiles=false&apikey={2}"
-    #data = {'monitored' : MonitorStatus, 'id' : movieID, 'movileFile' : { 'quality': {'quality' : { 'id' : qualityId }}}}
     data = {'monitored' : MonitorStatus, 'id' : movieID, 'qualityProfileId' : qualityId, 'Path' : moviePath }
     payload = {'json_payload': json.dumps(data)}
+
+    print(payload)
     rep = requests.put(apireq.format(RADARR_HOST, movieID, RADARR_API_KEY), data=json.dumps(data))
     print(rep.json())
+    syslog.syslog(syslog.LOG_ERR, str(movieID))
+    syslog.syslog(syslog.LOG_ERR, str(environ))
 
 def readConfig(config_file_path):
     with open(config_file_path, "r") as f:
         return json.load(f)
 
 # if a  json confile file argument is used, the local settings are overriden
-if os.path.isfile(sys.argv[1]):
-    try:
-        config = readConfig(sys.argv[1])
-        RADARR_API_KEY = config["RADARR_API_KEY"]
-        RADARR_HOST = config["RADARR_HOST"]
-    except:
-        print("An error occured while loading config file")
-        sys.exit(-1)
+if len(sys.argv) > 1:
+    if os.path.isfile(sys.argv[1]):
+        try:
+            config = readConfig(sys.argv[1])
+            RADARR_API_KEY = config["RADARR_API_KEY"]
+            RADARR_HOST = config["RADARR_HOST"]
+        except:
+            print("An error occured while loading config file")
+            sys.exit(-1)
 
 EventType = environ.get('sonarr_eventtype')
+movieId = 1254
 if EventType == 'Test':
     logging.info("Episode Unmonitor script testing")
 else:
-    mov = getMovie(3)
-    qualityProfileId = mov["qualityProfileId"]
-    moviePath = mov["path"]
-    setMonitoring(3, qualityProfileId, moviePath, False)
+    #movieId = environ.get("radarr_movie_id")
+    if movieId:
+        mov = getMovie(movieId)
+        print(mov)
+        qualityProfileId = mov["qualityProfileId"]
+        moviePath = mov["path"]
+        print(qualityProfileId)
+        print(moviePath)
+        setMonitoring(movieId, qualityProfileId, moviePath, False)
 
-    # epId = environ.get('sonarr_episodefile_episodeids')
-    # serieTitle = environ.get("sonarr_series_title")
-    # epSeason = int(environ.get("sonarr_episodefile_seasonnumber"))
-    # epNumber = int(environ.get("sonarr_episodefile_episodenumbers"))
-    # epTitle = environ.get("sonarr_episodefile_episodetitles")
-
-    
-
-    
-    
-    #logMsg = "Radarr post-import: '{0} S{1:0>2d}E{2:0>2d} {3}' unmonitored!"
     #logging.info(logMsg.format(serieTitle, epSeason, epNumber, epTitle))
