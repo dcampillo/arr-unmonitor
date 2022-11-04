35135b454830b77168788ef5805cef8354c0de85
David Campillo <2921284+dcampillo@users.noreply.github.com>
2022-11-04 13:13:19 +0100

    - release 0.8

--- radarr-unmonitor.py
+++ radarr-unmonitor.py
@@ -7,34 +7,56 @@
 # Description : Automatically unmonitor episde on "Import"
 ###################################
 
-import logging, json, ssl
+import logging, json, ssl, re, sys
 from urllib.request import Request, urlopen
+from urllib.error import HTTPError
 
 from os import environ, path
 
-RADARR_API_KEY = ""
-RADARR_HOST = ""
-REQ_HEADERS = {'X-Api-Key': RADARR_API_KEY, 'Content-Type': 'application/json'}
+ARR_API_KEY = ""
+ARR_HOST = ""
+REQ_HEADERS = {'X-Api-Key': ARR_API_KEY, 'Content-Type': 'application/json'}
+ARR_USE_SSL = False
 
 def getMovie(movieID):
-    apireq = "{0}/api/v3/movie/{1}".format(RADARR_HOST, movieID)
-    ssl._create_default_https_context = ssl._create_unverified_context
+    apireq = "{0}/api/v3/movie/{1}".format(ARR_HOST, movieID)
+    if ARR_USE_SSL:
+        ssl._create_default_https_context = ssl._create_unverified_context
+
     request = Request(method='GET', headers=REQ_HEADERS, url=apireq)
-    rep = urlopen(request)
-    MovieData = json.load(rep)
-    return MovieData
+    try:
+        rep = urlopen(request)
+        MovieData = json.load(rep)
+        return MovieData
+
+    except HTTPError as err:
+        sys.stderr.write("MovieID:{movieid} - HTTP{httpcode} - {reason}".format(movieid=movieID, httpcode=err.code, reason=err.reason))
+
+    except Exception as err:
+        print(err)
+        sys.exit("ERROR" )
 
 def setMonitoring(movieID, MonitoringStatus):
-    apireq = "{0}/api/v3/movie/{1}?moveFiles=false".format(RADARR_HOST, movieID)
+    apireq = "{0}/api/v3/movie/{1}?moveFiles=false".format(ARR_HOST, movieID)
     movieItem = getMovie(movieID)
     movieItem["monitored"] = MonitoringStatus
     request = Request(method='PUT', headers=REQ_HEADERS, data=json.dumps(movieItem).encode('utf-8'), url=apireq)
     rep = urlopen(request)
 
-EventType = environ.get('sonarr_eventtype')
+EventType = environ.get('radarr_eventtype')
 if EventType == 'Test':
-    logging.info("Movie Unmonitor script testing")
+    print("Checking config")
+    if ARR_API_KEY != "":
+        print("CONFIG_CHECK: API key is present")
+
+    if re.match("^https?:\\/\\/(?:www\\.)?[-a-zA-Z0-9@:%._\\+~#=]{1,256}\\.[a-zA-Z0-9()]{1,6}\\b(?:[-a-zA-Z0-9()@:%_\\+.~#?&\\/=]*)$", ARR_HOST):
+        print("CONFIG_CHECK: {hostcheck} is valid!".format(hostcheck=ARR_HOST))
+    else:
+        sys.stderr.write("CONFIG_CHECK_ERROR: {hostcheck} is NOT VALID!".format(hostcheck=ARR_HOST))
+        sys.exit("CONFIG_CHECK_ERROR")
+
 else:
     movieId = environ.get("radarr_movie_id")
     if movieId:
         setMonitoring(movieId, False)
+        print("Movie ID {movieid} unmonitored!".format(movieId=movieId))
--- sonarr-unmonitor.py
+++ sonarr-unmonitor.py
@@ -7,37 +7,67 @@
 # Description : Automatically unmonitor episde on "Import"
 ###################################
 
-import logging, json, ssl
+import logging, json, ssl, re, sys
 from urllib.request import Request, urlopen
-
+from urllib.error import HTTPError, URLError
 from os import environ, path
 
-SONARR_API_KEY = ""
-SONARR_HOST = ""
-REQ_HEADERS = {'X-Api-Key': SONARR_API_KEY, 'Content-Type': 'application/json'}
+ARR_API_KEY = ""
+ARR_HOST = ""
+REQ_HEADERS = {'X-Api-Key': ARR_API_KEY, 'Content-Type': 'application/json'}
+ARR_USE_SSL = False
 
 def getEpisode(episodeID):
-    apireq = "{0}/api/episode/{1}".format(SONARR_HOST, episodeID)
-    ssl._create_default_https_context = ssl._create_unverified_context
+    apireq = "{0}/api/episode/{1}".format(ARR_HOST, episodeID)
+    if ARR_USE_SSL:
+        ssl._create_default_https_context = ssl._create_unverified_context
     request = Request(method='GET', headers=REQ_HEADERS, url=apireq)
     rep = urlopen(request)
     epData = json.load(rep)
     return epData
 
 def setMonitoring(episodeID, MonitorStatus):
-    apireq = "{0}/api/episode/{1}".format(SONARR_HOST, episodeID)
-    episodeInfos = getEpisode(episodeID)
-    episodeInfos["monitored"] = MonitorStatus
-    request = Request(method='PUT', headers=REQ_HEADERS, data=json.dumps(episodeInfos).encode('utf-8'), url=apireq)
-    rep = urlopen(request)
+    apireq = "{0}/api/episode/{1}".format(ARR_HOST, episodeID)
+    try:
+        episodeInfos = getEpisode(episodeID)
+        if episodeInfos:
+            episodeInfos["monitored"] = MonitorStatus
+            request = Request(method='PUT', headers=REQ_HEADERS, data=json.dumps(episodeInfos).encode('utf-8'), url=apireq)
+            rep = urlopen(request)
+            sys.stdout.write("SONARR_UNMONITOR: EpisodeID:{epid} - Unmonitored".format(epid=episodeID))
+        else:
+            print("Episode not found")
 
-EventType = environ.get('sonarr_eventtype')
-if EventType == 'Test':
-    logging.info("Episode Unmonitor script testing")
-else:
-    epId = environ.get('sonarr_episodefile_episodeids')
-    if epId:
-        setMonitoring(epId, False)
-        logMsg = "Sonarr post-import: Episode ID = {0}"
-        logging.info(logMsg.format(epId))
+    except HTTPError as err:
+        sys.stderr.write("SONARR_UNMONITOR: EpisodeID:{episodeID} - HTTP{httpcode} - {reason}".format(episodeID=episodeID, httpcode=err.code, reason=err.reason))
+        sys.exit(1)
 
+    except URLError as err:
+        sys.stderr.write("SONARR_UNMONITOR: EpisodeID:{episodeID} - Error: {reason} | Check script configuration ARR_HOST setting".format(episodeID=episodeID, reason=err.reason))
+        sys.exit(1)
+
+    except Exception as err:
+        sys.stderr.write("SONARR_UNMONITOR: Unknow Error RRRRRRRRRR")
+
+def main():
+
+    EventType = environ.get('sonarr_eventtype')
+    if EventType == 'Test':
+        print("Checking config")
+        if ARR_API_KEY != "":
+            print("CONFIG_CHECK: API key is present")
+
+            if re.match("^https?:\\/\\/(?:www\\.)?[-a-zA-Z0-9@:%._\\+~#=]{1,256}\\.[a-zA-Z0-9()]{1,6}\\b(?:[-a-zA-Z0-9()@:%_\\+.~#?&\\/=]*)$", ARR_HOST):
+                print("CONFIG_CHECK: {hostcheck} is valid!".format(hostcheck=ARR_HOST))
+            else:
+                sys.stderr.write("CONFIG_CHECK_ERROR: {hostcheck} is NOT VALID!".format(hostcheck=ARR_HOST))
+                sys.exit("CONFIG_CHECK_ERROR")
+    else:
+        epId = environ.get('sonarr_episodefile_episodeids')
+        if epId:
+            setMonitoring(epId, False)
+            logMsg = "Sonarr post-import: Episode ID = {0}"
+            logging.info(logMsg.format(epId))
+
+if __name__ == "__main__":
+    main()
